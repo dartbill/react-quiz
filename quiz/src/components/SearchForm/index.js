a43016622e2c0dcc09d8c7110836fd0712a8f954
Tom Hughes <tomhughes1987@gmail.com>
2022-06-27 17:39:29 +0200

    WIP forms, dropdown lists

--- quiz/src/components/SearchForm/index.js
+++ quiz/src/components/SearchForm/index.js
@@ -1,15 +1,44 @@
 import React, { useEffect } from "react";
+import { useSelector, useDispatch } from 'react-redux';
+
+import './index.css'
+
 
 
 const SearchForm = () => {
-
-
     // GetQuestions({ cat: 20, level: "easy", type: "multiple" })
+        const dispatch = useDispatch()
+        const handleBtnClick = () => {
+            dispatch ({ type: "DESPOSIT", payload: 100})
+        }
+      return (
+        <>
 
-    return (
-        <p>hello from component </p>
+{/* Ending up with loads of classNames */}
+<div className="dropdown">
+  <button className="dropbtn-diff">Difficult</button>
+  <div className="dropdown-content-diff">
+    <a className="drop-items-diff drop-items drop-items-easy" href="#">easy </a>
+    <a className="drop-items-diff drop-items drop-items-easy" href="#">medium</a>
+  </div>
+
+</div>
+
+
+
+{/* THIS DOESNT WORK??? */}
+{/* 
+<form action="/action_page.php">
+  <input list="browsers">
+  <datalist id="browsers">
+    <option value="Internet Explorer">
+    <option value="Firefox">
+    <option value="Chrome">
+    <option value="Opera">
+    <option value="Safari">
+  </datalist>
+</form> */}
+
     )
-
 }
-
 export default SearchForm
