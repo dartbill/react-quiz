5162e845997cc2fef6ffff420e9a6175cf5a1dfc
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-06-27 11:24:32 +0100

    WIP

--- quiz/src/test/App.test.js
+++ quiz/src/test/App.test.js
@@ -1,13 +1,21 @@
 import App from '../App';
-// import {renderComponent} from './setupTests'
+import {renderComponent} from './setupTests'
 import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import {store} from "../store"
 import '@testing-library/jest-dom'
 
+// export const render = component => rtlRender(
+//     {component}
+// )
+
 describe('Test App Component',()=>{
   beforeAll(()=>{
-    render(<App />);
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
   })
 
   it('renders header',()=>{
