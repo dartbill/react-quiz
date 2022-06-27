5ba2dc83eac14f14d53ebd5887d0256c76b2b294
dartbill <billieldartnell@hotmail.com>
2022-06-27 09:47:30 +0100

    add basic actions

--- quiz/src/reducers/reducer.js
+++ quiz/src/reducers/reducer.js
@@ -1,11 +1,29 @@
 
 const initialState = {
-
+    quiz: {
+        category: "",
+        type: "",
+        difficulty: "",
+        question: "",
+        correct_answer: "",
+        incorrect_answers: []
+    }
 }
 
 
 const reducer = (state = initialState, action) => {
 
+    switch (action.type) {
+        case "QUESTIONS":
+            return {
+                ...state, quiz: {
+                    ...state.quiz,
+                    ...action.payload
+                }
+            }
+        default:
+            return initialState
+    }
 
 }
 
