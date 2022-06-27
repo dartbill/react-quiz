6873d1146e68b4f90a5e7fc21d158c91ac875dcf
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-06-27 10:02:34 +0100

    add initial imports and test describe for setupTests.js, App.test.js

--- quiz/src/test/setupTests.js
+++ quiz/src/test/setupTests.js
@@ -1,2 +1,16 @@
-
+// Main Imports
+import React from 'react'
 import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import axios from 'axios';
+import userEvent from '@testing-library/user-event';
+
+// Import Reducers
+
+// Globals
+global.React = React;
+global.render = render;
+global.userEvent = userEvent;
