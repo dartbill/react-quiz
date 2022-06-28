265fc7c2768f61fba32b56fc908e4608b915b56d
Rebecca Blixt <rebecca.blixt@hotmail.com>
2022-06-28 12:39:23 +0200

    debug socket server side

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const app = express();
 const cors = require('cors');
 
@@ -8,5 +9,14 @@ app.use(cors());
 app.use('/scoreboard', scoreBoardRoutes);
 
 app.get('/', (req, res) => res.send('Quiz time!'));
+
+app.get('/highscores', (req, res) =>
+	res.send('Keep playing, and hopefully your name will be on the scoreboard!')
+);
+
+app.post('/login', (req, res) => {});
+
+// post username + score/login stuff
+// post scores
 
 module.exports = app;
