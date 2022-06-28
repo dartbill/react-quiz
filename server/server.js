2b79ad9e715fc48b1b409bb09ffdf06fe0124872
Rebecca Blixt <rebecca.blixt@hotmail.com>
2022-06-28 18:47:14 +0200

    add scoreboardroutes to server.js

--- server/server.js
+++ server/server.js
@@ -5,6 +5,10 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
+const scoreBoardRoutes = require('./routes/scoreBoard');
+
+server.use('/scoreboard', scoreBoardRoutes);
+
 server.get('/', (req, res) => res.send('Welcome to the library'));
 
 module.exports = server;
