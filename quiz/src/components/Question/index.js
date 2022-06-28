868d4de6c9fc6e9ed487f815690f5fc8dfff2a28
dartbill <billieldartnell@hotmail.com>
2022-06-28 07:58:23 +0100

    add question to screen

--- quiz/src/components/Question/index.js
+++ quiz/src/components/Question/index.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Answer } from '../'
+import { useSelector } from 'react-redux';
+
 
 export const Question = () => {
+
+  const q = useSelector(state => state.questions)
+  console.log(q)
+
   return (
     <div>
 
-      <div className="question">This will be the Question?</div>
+      <div className="question">{q[1].question}</div>
       <div className="answers">
         <Answer />
         <Answer />
