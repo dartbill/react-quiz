69c2cc6c09ddcbba97d78dbe25cbd39c22233600
Tom Hughes <91187363+tomhughes87@users.noreply.github.com>
2022-06-28 17:01:52 +0200

    add className to navlink button

--- quiz/src/pages/Home/index.js
+++ quiz/src/pages/Home/index.js
@@ -25,7 +25,7 @@ export const Home = () => {
     return (
         <>
             <SearchForm />
-            <NavLink to="/quiz">
+            <NavLink className="ourBtns" to="/quiz">
                 Play!
             </NavLink>
         </>
