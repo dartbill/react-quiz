c3f8d782d1d1cc0739677c8833124b2741cd1d40
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-06-28 12:23:46 +0100

    WIP

--- quiz/src/pages/Home/Home.test.js
+++ quiz/src/pages/Home/Home.test.js
@@ -1,50 +1,68 @@
 import "@testing-library/jest-dom";
 import { Home } from ".";
 import { screen, render } from "@testing-library/react";
-import { default as store } from "../../store";
 import { renderWithReduxProvider } from '../../test/setupTests'
+import userEvent from "@testing-library/user-event";
+import mockStore from 'redux-mock-store'
 
+let initState = {
+    category: 20,
+    level: "easy",
+    typeOfQ: "multiple",
+    questions: [],
+};
+
+let getResultMock;
+let store;
 
 describe("Home Component", () => {
-    beforeAll(() => {
-        let initState = {
-            category: 20,
-            level: "easy",
-            typeOfQ: "multiple",
-            questions: [],
-        };
+    beforeEach(() => {
+        getResultMock = jest.fn();
+        store = mockStore(initState)
         renderWithReduxProvider(<Home />, { initState });
     });
 
-    describe("Test Layout", () => {
-        test("Renders heading", () => {
-            const heading = screen.getByRole("heading", {
-                name: /hello/i,
-            });
-            expect(heading).toBeInTheDocument();
+    describe("Renders", () => {
+        
+        it("category dropdown", () => {
+            const btn = screen.getByRole('button', {  name: /category/i})
+            expect(btn).toBeInTheDocument();
         });
+        it("difficult dropdown", () => {
+            const btn = screen.getByRole('button', {  name: /difficult/i})
+            expect(btn).toBeInTheDocument();
+        });
+
     });
 
-    describe("Test Reducer", () => {
+    describe("Updates Store", () => {
         // console.log("store", store.getState());
-        it("Returns initial state", () => {
-            expect(store.getState()).toEqual(initialState);
+        it("initial state", () => {
+            expect(store).toEqual(initState);
         });
 
-        it("Returns for case SETCAT", () => {
-            // test ui button for
-            expect(store.getState().category).toEqual(10);
+        it("for case SETCAT", async () => {
+            // const btn = screen.getByRole('button', {  name: /category/i})
+            // userEvent.click(btn)
+            const link = screen.getByRole('link', {  name: /general knowledge/i})
+            userEvent.click(link)
+            console.log(store)
+            expect(link).toHaveBeenCalled(getResultMock)
+            // expect(store.category).toEqual(9);
         });
-        it("Returns for case SETLEVEL", () => {
-            // test ui button for cat
-            expect(store.getState().level).toEqual("hard");
-        });
-        it("Returns for case SETTYPE", () => {
-            // test ui button for cat
-            expect(store.getState().typeOfQ).toEqual("boolean");
-        });
-        it("Returns for case TEST", () => {
 
-        });
+        // it("Returns for case SETLEVEL", () => {
+        //     // test ui button for cat
+        //     expect(store.getState().level).toEqual("hard");
+        // });
+        // it("Returns for case SETTYPE", () => {
+        //     // test ui button for cat
+        //     expect(store.getState().typeOfQ).toEqual("boolean");
+        // });
+        // it("Returns for case TEST", () => {
+
+        // });
     });
 });
+
+
