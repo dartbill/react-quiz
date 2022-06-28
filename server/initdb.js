b33b9f9565bf297469f830f3df680f4f1d25713f
Rebecca Blixt <rebecca.blixt@hotmail.com>
2022-06-28 16:26:28 +0200

    change Pool for debug

--- server/initdb.js
+++ server/initdb.js
@@ -1,5 +1,6 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({ database: process.env.PGDATABASE });
+// const pool = new Pool({ database: process.env.PGDATABASE });
+const pool = new Pool();
 
 module.exports = pool;
