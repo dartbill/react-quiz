0cf4978c2b235a9d0bc9fdbb4946bb81c2a1c3d2
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-06-28 07:46:53 +0100

    WIP

--- quiz/src/test/setupTests.js
+++ quiz/src/test/setupTests.js
@@ -71,51 +71,7 @@ const renderWithReduxProvider = (ui, options={}) => {
 jest.mock('axios')
 axios.get.mockResolvedValue({ data: [ { latlng: [123, 456] }]})
 
-global.renderWithReduxProvider = renderWithReduxProvider
-
-
-
-
-
-// function renderWithRedux(
-//     component,
-//     { store = createStore(reducer, applyMiddleware(thunk)) } = {}
-// ) {
-//     return {
-//         ...render(<Provider store={store}>{component}</Provider>),
-//     };
-// }
-
-// const TestProviders = ({ initState }) => {
-//   // initState ||= { location: "", result: { sunrise: "", sunset: "" }, loading: false };
-//   let testReducer = () => reducer(initState, { type: '@@INIT' })
-//   const testStore =
-
-//   return ({ children }) => (
-//       <Provider store={testStore}>
-//           { children }
-//       </Provider>
-//   )
-// }
-
-// const renderWithReduxProvider = (ui, options={}) => {
-//   let TestWrapper = TestProviders(options)
-//   render(ui, { wrapper: TestWrapper, ...options })
-// }
-
-// import axios from 'axios';
-// jest.mock('axios')
-// axios.get.mockResolvedValue({ data: [ { latlng: [123, 456] }]})
-
-// const localStorageMock = {
-//   getItem: jest.fn(),
-//   setItem: jest.fn(),
-//   removeItem: jest.fn(),
-//   clear: jest.fn(),
-// };
-
 // Globals
-// global.localStorage = localStorageMock;
 global.React = React;
 global.userEvent = userEvent;
-// global.renderWithRedux = renderWithRedux;
+global.renderWithReduxProvider = renderWithReduxProvider
