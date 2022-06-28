69a199bc74c5c309ff876d4c46c36ec67a158fb2
dartbill <billieldartnell@hotmail.com>
2022-06-28 10:39:07 +0100

    add highscore page

---
+++ quiz/src/components/Highscores/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Highscores = () => {
+
+  //fetching and sorting out highscores to be done here
+
+
+  return (
+    <div>
+      <p>We need to display highscores here</p>
+    </div>
+
+  )
+}
--- quiz/src/pages/index.js
+++ quiz/src/pages/index.js
@@ -1,3 +1,4 @@
 export { Home } from './Home'
 export { QuizPage } from './Quiz'
 export { LoginPage } from './Login'
+export { HighscorePage } from './Highscores'
