0fe4476dd50d09ff51e6a1bc1bfb9cdd469ac1fe
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-06-29 19:33:29 +0100

    add StartGame button to replace Play NavLink

---
+++ quiz/src/components/StartGameButton/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+export const StartGameButton = () => {
+    let navigate = useNavigate();
+    const routeChange = (path) => {
+        navigate(path);
+    };
+
+    const playerCount = useSelector((state) => state.playerCount);
+    const player1 = useSelector((state) => state.player1);
+    const player2 = useSelector((state) => state.player2);
+
+    const startGame = () => {
+        const path = "/quiz";
+        switch (playerCount) {
+            case 2:
+                if (player1.username && player2.username) {
+                    routeChange(path);
+                } else {
+                    window.alert("Please save a username for both players");
+                }
+                break;
+
+            default:
+                if (player1.username) {
+                  console.log(player1.username)
+                    routeChange(path);
+                } else {
+                    window.alert("Please save a username");
+                }
+                break;
+        }
+    };
+
+    return <button onClick={startGame}>Play!</button>;
+};
