1bdfdbf4c73b4bac78ac534ebc04c3f811d213cd
dartbill <billieldartnell@hotmail.com>
2022-06-29 12:03:18 +0100

    remove id

--- server/db/migrations/1_setup.sql
+++ server/db/migrations/1_setup.sql
@@ -1,7 +1,6 @@
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
-  id serial PRIMARY KEY,
   name varchar(50) NOT NULL,
   score int NOT NULL
 );
--- server/models/Score.js
+++ server/models/Score.js
@@ -43,8 +43,7 @@ module.exports = class Score {
 	static create(name, score) {
 		return new Promise(async (resolve, reject) => {
 			try {
-				const newId = 10
-				let result = await db.query('INSERT INTO users (id, name, score) VALUES ($1, $2, $3);', [newId, name, score]);
+				let result = await db.query('INSERT INTO users (name, score) VALUES ($1, $2);', [name, score]);
 				const user = new Score(result.rows[0]);
 				resolve(user)
 			} catch (err) {
