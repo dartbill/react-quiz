958613791379d38266446b97af1fdc6c417d89ed
dartbill <billieldartnell@hotmail.com>
2022-06-29 15:22:31 +0100

    80% coverage

--- server/_tests_/unit/controllers/scoreboard.test.js
+++ server/_tests_/unit/controllers/scoreboard.test.js
@@ -24,4 +24,36 @@ describe('scores controller', () => {
       expect(mockJson).toHaveBeenCalledWith(testScores);
     });
   });
+  describe('getUser', () => {
+    test('it returns a user with a 200 status code', async () => {
+      let testUser = {
+        id: 1,
+        name: 'Test user',
+        score: 10
+      }
+      jest.spyOn(Score, 'findByUsername')
+        .mockResolvedValue(new Score(testUser));
+
+      const mockReq = { params: { id: 1 } }
+      await scoreBoardController.getUser(mockReq, mockRes);
+      expect(mockStatus).toHaveBeenCalledWith(200);
+      expect(mockJson).toHaveBeenCalledWith(new Score(testUser));
+    })
+  });
+
+  describe('addUser', () => {
+    test('it returns a new user with a 201 status code', async () => {
+      let testUser = {
+        name: 'Test user',
+        score: 10
+      }
+      jest.spyOn(Score, 'create')
+        .mockResolvedValue(new Score(testUser));
+
+      const mockReq = { body: testUser }
+      await scoreBoardController.addUser(mockReq, mockRes);
+      expect(mockStatus).toHaveBeenCalledWith(200);
+      // expect(mockJson).toHaveBeenCalledWith(new Score(testUser));
+    })
+  });
 });
