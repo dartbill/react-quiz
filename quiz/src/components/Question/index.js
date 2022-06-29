add983c2eea648e2ccf4e13de1236525918b6847
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-06-29 22:35:38 +0100

    WIP

--- quiz/src/components/Question/index.js
+++ quiz/src/components/Question/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Answer } from "../";
 
-export const Question = ({questionType,turn,question,nextQuestion}) => {
+export const Question = ({index,turn,onClick}) => {
 
   // TOMS STUFF FOR BETTING///////////////////////////////////////////
       const [orderA, setOrderA] = useState("0");
@@ -11,40 +11,54 @@ export const Question = ({questionType,turn,question,nextQuestion}) => {
 
       function createRandOrder() {
           const rand1 = Math.floor(Math.random() * (4 - 0 + 1) + 0);
-          setOrderA("bx" + rand1);
           const rand2 = Math.floor(Math.random() * (4 - 0 + 1) + 0);
-          setOrderB("bx" + rand2);
           const rand3 = Math.floor(Math.random() * (4 - 0 + 1) + 0);
-          setOrderC("bx" + rand3);
           const rand4 = Math.floor(Math.random() * (4 - 0 + 1) + 0);
+          setOrderA("bx" + rand1);
+          setOrderB("bx" + rand2);
+          setOrderC("bx" + rand3);
           setOrderD("bx" + rand4);
       }
   // End of TOMS STUFF FOR BETTING///////////////////////////////////////////
 
+    // qustion data
+    const questions = useSelector((state) => state.questions);
+
+    let question;
+    let correctAnswer;
+    let incorrectAnswers = [];
+    let questionType;
+
+    if (questions.length !== 0) {
+        question = questions[question].question;
+        incorrectAnswers = questions[question].incorrect_answers;
+        correctAnswer = questions[question].correct_answer;
+        questionType = questions[question].type;
+    }
+
+
     return (
         <>
             <div className="quiz">
-                <div className="question">{quest}</div>
+                <div className="question">{question}</div>
                 {questionType === "boolean" ? (
                     <div className="Container-Answers">
                         <div className="genBtn correctBtn bx3" id={orderA}>
                             <Answer
-                                question={question}
                                 onClick={nextQuestion}
                                 turn={turn}
                                 bool={true}
                                 num="A"
-                                answer={correct}
+                                answer={correctAnswer}
                             />
                         </div>
                         <div className="genBtn wrongBtn bx4" id={orderB}>
                             <Answer
-                                question={question}
                                 onClick={nextQuestion}
                                 turn={turn}
                                 bool={false}
                                 num="B"
-                                answer={incorrect[0]}
+                                answer={incorrectAnswers[0]}
                             />
                         </div>
                     </div>
@@ -52,42 +66,38 @@ export const Question = ({questionType,turn,question,nextQuestion}) => {
                     <div className="Container-Answers" onLoad={createRandOrder}>
                         <div className="genBtn correctBtn bx3" id={orderA}>
                             <Answer
-                                question={question}
                                 onClick={nextQuestion}
                                 turn={turn}
                                 bool={true}
                                 num="A"
-                                answer={correct}
+                                answer={correctAnswer}
                             />
                         </div>
                         <div className="genBtn wrongBtn bx4" id={orderB}>
                             <Answer
-                                question={question}
                                 onClick={nextQuestion}
                                 turn={turn}
                                 bool={false}
                                 num="B"
-                                answer={incorrect[0]}
+                                answer={incorrectAnswers[0]}
                             />
                         </div>
                         <div className="genBtn wrongBtn bx1" id={orderC}>
                             <Answer
-                                question={question}
                                 onClick={nextQuestion}
                                 turn={turn}
                                 bool={false}
                                 num="C"
-                                answer={incorrect[1]}
+                                answer={incorrectAnswers[1]}
                             />
                         </div>
                         <div className="genBtn wrongBtn bx2" id={orderD}>
                             <Answer
-                                question={question}
                                 onClick={nextQuestion}
                                 turn={turn}
                                 bool={false}
                                 num="D"
-                                answer={incorrect[2]}
+                                answer={incorrectAnswers[2]}
                             />
                         </div>
                     </div>
--- quiz/src/components/Quiz/index.js
+++ quiz/src/components/Quiz/index.js
@@ -1,112 +1,94 @@
 import React, { useReducer, useState } from "react";
 import { Question } from "../Question";
-import '../.././index.css'
+import "../.././index.css";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 const initialState = {
-  currentQuestionIndex: 0,
+    currentQuestionIndex: 0,
 };
 
 const reducer = (state, action) => {
-  if (action.type === "NEXT_QUESTION") {
-    return {
-      ...state,
-      currentQuestionIndex: state.currentQuestionIndex + 1,
-    };
-  }
-  return state;
+    if (action.type === "NEXT_QUESTION") {
+        return {
+            ...state,
+            currentQuestionIndex: state.currentQuestionIndex + 1,
+        };
+    }
+    return state;
 };
 
 export const Quiz = () => {
-  // eslint-disable-next-line
-  let navigate = useNavigate();
-  const routeChange = (path) => {
-    navigate(path);
-  }
-  const [state, dispatch] = useReducer(reducer, initialState);
-  const [turn, setTurn] = useState(true)
+    // eslint-disable-next-line
+    let navigate = useNavigate();
+    const routeChange = (path) => {
+        navigate(path);
+    };
+    const [state, dispatch] = useReducer(reducer, initialState);
+    const [turn, setTurn] = useState(true);
 
-  let [score1, setScore1] = useState(0)
-  let [score2, setScore2] = useState(0)
-  const player1 = useSelector((state) => state.player1);
-  const player2 = useSelector((state) => state.player1);
- 
-  const nextQuestion = (e) => {
-    // Updates Score
-    updateScore();
+    let [score1, setScore1] = useState(0);
+    let [score2, setScore2] = useState(0);
+    const player1 = useSelector((state) => state.player1);
+    const player2 = useSelector((state) => state.player1);
 
-    // Updates Question to Next Question
-    state.currentQuestionIndex < 19 
-     ? dispatch({ type: "NEXT_QUESTION" }) 
-     : routeChange('/final');
+    const nextQuestion = (e) => {
+        // Updates Score
+        updateScore();
 
-    // Updates Turn
-    updateTurn(e);
-   }
+        // Updates Question to Next Question
+        state.currentQuestionIndex < 19
+            ? dispatch({ type: "NEXT_QUESTION" })
+            : routeChange("/final");
+        // Updates Turn
+        updateTurn(e);
+    };
 
-   const updateScore = (e) => {
-    e.preventDefault();
-    if (turn) {
-      setScore1(score1 = score1 + 1)
-      dispatch({
-        type: 'SET_PLAYER1',
-        payload: {
-          score: score1
+    const updateScore = (e) => {
+        e.preventDefault();
+        if (turn) {
+            setScore1((score1 = score1 + 1));
+            dispatch({
+                type: "SET_PLAYER1",
+                payload: {
+                    score: score1,
+                },
+            });
+        } else {
+            setScore2((score2 = score2 + 1));
+            dispatch({
+                type: "SET_PLAYER2",
+                payload: {
+                    score: score2,
+                },
+            });
         }
-      })
-    } else {
-      setScore2(score2 = score2 + 1)
-      dispatch({
-        type: 'SET_PLAYER2',
-        payload: {
-          score: score2
+        console.log("this is score2 " + score2);
+        console.log("this is player2 " + player2.score);
+    };
+
+    const updateTurn = (e) => {
+        e.preventDefault(e);
+        if (turn) {
+            setTurn(false);
+        } else {
+            setTurn(true);
         }
-      })
-    }
-    console.log("this is score2 " + score2)
-    console.log("this is player2 " + player2.score)
-  }
+    };
 
-  const updateTurn = (e) => {
-    e.preventDefault(e)
-    if (turn) {
-      setTurn(false)
-    } else {
-      setTurn(true)
-    }
-  }
-
-    // qustion data
-    const questions = useSelector((state) => state.questions);
-
-    let question;
-    let correctAnswers;
-    let incorrectAnswers = [];
-    let questionType;
-
-    if (questions.length !== 0) {
-        question = questions[question].question;
-        incorrectAnswers = questions[question].incorrect_answers;
-        correctAnswers = questions[question].correct_answer;
-        questionType = questions[question].type;
-    }
-
-  return (
-    <div className="quiz">
-      <div>
-        <div className="score">
-          {`Question ${state.currentQuestionIndex + 1}/10`}
-          </div>
-        <Question 
-          questionType={type}
-          turn={turn}
-          onClick={nextQuestion}
-        />
-        <br />
-      </div>
-    </div>
-  );
+    return (
+        <div className="quiz">
+            <div>
+                <div className="score">
+                    {`Question ${state.currentQuestionIndex + 1}/10`}
+                </div>
+                <Question
+                    index={state.currentQuestionIndex}
+                    turn={turn}
+                    onClick={nextQuestion}
+                />
+                <br />
+            </div>
+        </div>
+    );
 };
-
-
