25bdc9719aa1406f0fb9edeaa0ff3a78f930f448
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-06-29 19:30:02 +0100

    cleanup code in QuizPage

--- quiz/src/pages/QuizPage/index.js
+++ quiz/src/pages/QuizPage/index.js
@@ -1,29 +1,21 @@
-import React, { useEffect } from 'react';
-import { Quiz } from '../../components';
+import React, { useEffect } from "react";
+import { Quiz } from "../../components";
 // SearchForm?
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 
-import { GetQuestions } from '../../actions';
+import { GetQuestions } from "../../actions";
 
 export const QuizPage = () => {
-	const dispatch = useDispatch();
-	// const data = useSelector((state) => state.questions);
-	// console.log(data);
+    const dispatch = useDispatch();
+    const category = useSelector((state) => state.category);
+    let level = useSelector((state) => state.level);
+    let typeOfQ = useSelector((state) => state.typeOfQ);
 
-	const category = useSelector((state) => state.category);
-	let level = useSelector((state) => state.level);
-	let typeOfQ = useSelector((state) => state.typeOfQ);
+    useEffect(() => {
+        dispatch(
+            GetQuestions({ cat: category, level: level, typeOfQ: typeOfQ })
+        );
+    }, [category, level, typeOfQ, dispatch]);
 
-	useEffect(() => {
-		dispatch(GetQuestions({ cat: category, level: level, typeOfQ: typeOfQ }));
-	}, [category, level, typeOfQ, dispatch]);
-
-	return (
-		<>
-			<h2>Quiz Page</h2>
-			{/* <h1>Hi, this is the quiz page</h1>
-            <SearchForm /> */}
-			<Quiz />
-		</>
-	);
+    return <Quiz />;
 };
