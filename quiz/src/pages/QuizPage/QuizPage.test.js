d7fe9f0d5d1571f2cd9886be9db0a434bc4b53b4
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-06-29 07:00:25 +0100

    remove old quiz file, add to QuizPage.test WIP

--- quiz/src/pages/QuizPage/QuizPage.test.js
+++ quiz/src/pages/QuizPage/QuizPage.test.js
@@ -3,9 +3,33 @@ import { render, screen } from '@testing-library/react';
 import "@testing-library/jest-dom";
 import React from "react";
 
+const initialState = {
+    category: 20,
+    level: "easy",
+    typeOfQ: "multiple",
+    questions: [],
+};
+
+const TestProviders = ({ initState }) => {
+    initState ||= { initialState };
+    let testReducer = () => reducer(initState, { type: '@@INIT' })
+    const testStore = createStore(testReducer, applyMiddleware(thunk))
+
+    return ({ children }) => (
+        <Provider store={testStore}>
+            { children }
+        </Provider>
+    )
+}
+
+const renderWithReduxProvider = (ui, options={}) => {
+    let TestWrapper = TestProviders(options)
+    render(ui, { wrapper: TestWrapper, ...options })
+}
+
 describe("Quiz Component", () => {
     beforeEach(() => {
-        render(
+        renderWithReduxProvider(
             <QuizPage />
         );
     });
