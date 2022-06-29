10a1e1be3919a77024d1def57f3b655e6262fc58
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-06-29 17:01:08 +0100

    add NewGameButton

--- quiz/src/components/NewGameButton/index.js
+++ quiz/src/components/NewGameButton/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useNavigate } from "react-router-dom";
 
-export const NewGame = () => {
+export const NewGameButton = () => {
 
   let navigate = useNavigate();
   const routeChange = (path) => {
