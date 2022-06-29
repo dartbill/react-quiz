c507925ae2398a143f1708bd7c6ba05c5228123a
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-06-29 16:34:55 +0100

    add new game button to finalscore page

--- quiz/src/pages/FinalScorePage/index.js
+++ quiz/src/pages/FinalScorePage/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { FinalScores } from "../../components";
+import { NewGame } from '../../components'
 
 export const FinalScorePage = () => {
-
 
     return (
         <>
         <h2>Final Scores</h2>
             <FinalScores />
+            <NewGame/>
         </>
     )
 }
