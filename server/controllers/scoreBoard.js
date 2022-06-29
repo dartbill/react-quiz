e36a07e778c69e6fa487e2c511e4420dfbbd53a7
dartbill <billieldartnell@hotmail.com>
2022-06-29 09:39:31 +0100

    add err var

--- server/controllers/scoreBoard.js
+++ server/controllers/scoreBoard.js
@@ -4,36 +4,36 @@ async function index(req, res) {
 	try {
 		const scoreBoard = await Score.all;
 		res.status(200).json(scoreBoard);
-	} catch {
+	} catch (err) {
 		console.error(err);
 		res.status(500).json({ err });
 	}
 }
 
-async function show(req, res) {
-	try {
-		const score = await Score.findById(parseInt(req.params.id));
-		console.log(score);
-		res.status(200).json(score);
-	} catch {
-		console.error(err);
-		res.status(404).json({ err });
-	}
-}
+// async function show(req, res) {
+// 	try {
+// 		const score = await Score.findById(parseInt(req.params.id));
+// 		console.log(score);
+// 		res.status(200).json(score);
+// 	} catch {
 
-async function create(req, res) {
-	try {
-		const score = await Score.create(
-			req.body.id,
-			req.body.name,
-			req.body.score
-		);
-		console.log(score);
-		res.status(200).json(score);
-	} catch {
-		console.error(err);
-		res.status(404).json({ err });
-	}
-}
+// 		res.status(404).json({ err });
+// 	}
+// }
+
+// async function create(req, res) {
+// 	try {
+// 		const score = await Score.create(
+// 			req.body.id,
+// 			req.body.name,
+// 			req.body.score
+// 		);
+// 		console.log(score);
+// 		res.status(200).json(score);
+// 	} catch {
+// 		console.error(err);
+// 		res.status(404).json({ err });
+// 	}
+// }
 
 module.exports = { index, show, create };
