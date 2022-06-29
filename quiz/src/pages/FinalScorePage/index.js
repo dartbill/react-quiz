fa6c547d23e65eb7784accda1514504721de2033
Rebecca Blixt <rebecca.blixt@hotmail.com>
2022-06-29 18:56:50 +0200

    add react-confetti

--- quiz/src/pages/FinalScorePage/index.js
+++ quiz/src/pages/FinalScorePage/index.js
@@ -1,16 +1,16 @@
-import React from "react";
-import { FinalScores } from "../../components";
+import React from 'react';
+import { FinalScores } from '../../components';
+
+import useWindowSize from 'react-use/lib/useWindowSize';
+import Confetti from 'react-confetti';
 
 export const FinalScorePage = () => {
-
-
-    return (
-        <>
-        <h2>Final Scores</h2>
-            <FinalScores />
-        </>
-    )
-}
-
-
-
+	const { width, height } = useWindowSize();
+	return (
+		<>
+			<h2>Final Scores</h2>
+			<Confetti width={width} height={height} />
+			<FinalScores />
+		</>
+	);
+};
