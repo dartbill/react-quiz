44855640a71a1efbe6258264e717e1e68b19926b
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-06-29 14:01:20 +0100

    add control flor code to Question component for button rendering

--- quiz/src/components/Question/index.js
+++ quiz/src/components/Question/index.js
@@ -16,24 +16,32 @@ export const Question = (props) => {
     quest = q[props.question].question
     incorrect = q[props.question].incorrect_answers
     correct = q[props.question].correct_answer
+    type = q[props.question].typeOfQ
     console.log('this is quest' + incorrect + correct)
   }
 
+  // boolean
+  // multiple
 
   //need to go to the next question when an answer is chosen
 
   return (
     <div>
-
       <div className="question">{quest}</div>
-      <div className="answers">
-        <Answer bool={true} num="A" answer={correct} />
+        { 
+        type === 'boolean' ? 
+        <div className="answers"> 
+        <Answer bool={true} num="A" answer={correct} /> 
         <Answer bool={false} num="B" answer={incorrect[0]} />
-        <Answer bool={false} num="C" answer={incorrect[1]} />
-        <Answer bool={false} num="D" answer={incorrect[2]} />
-      </div>
-
+        </div>
+        : 
+        <div className="answers"> 
+        <Answer bool={true} num="A" answer={correct} /> 
+        <Answer bool={false} num="B" answer={incorrect[0]} /> 
+        <Answer bool={false} num="C" answer={incorrect[1]} /> 
+        <Answer bool={false} num="D" answer={incorrect[2]} /> 
+        </div>
+        }
     </div>
   )
 }
-
