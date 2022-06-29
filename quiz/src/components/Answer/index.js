2667af14dc9cda6ae2723b664b684b9e6d08a4cd
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-06-29 23:45:24 +0100

    Update props in Answer component, add comment

--- quiz/src/components/Answer/index.js
+++ quiz/src/components/Answer/index.js
@@ -1,16 +1,20 @@
 import React from "react";
 
-export const Answer = ({ onNextQuestion,bool }) => {
+export const Answer = ({ onSubmitQuestion, bool, answer, num }) => {
+
+    // onClick calls function from Question 
+    // passes in bool (true or false) in order to allow/disallow update score
+    
     return (
         <div className="answer">
             <button
-                onClick={() => onNextQuestion(bool)}
+                onClick={ () => onSubmitQuestion(bool) }
                 className="answer-letter"
                 style={{ cursor: "pointer" }}
             >
                 {num}
             </button>
-            <div className="answer-text"> {answer} </div>
+            <div className="answer-text"> { answer } </div>
         </div>
     );
 };
