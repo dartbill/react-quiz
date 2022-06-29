d4a116dcd2dcfe8a57b43eb114166667db6f422c
dartbill <billieldartnell@hotmail.com>
2022-06-29 11:47:30 +0100

    test

--- server/models/Score.js
+++ server/models/Score.js
@@ -42,7 +42,7 @@ module.exports = class Score {
 	static create(name, score) {
 		return new Promise(async (resolve, reject) => {
 			try {
-				const result = await db.query('INSERT INTO users (name, score) VALUES ($2, $3) RETURNING *;', [name, score]);
+				let result = await db.query('INSERT INTO users (name, score) VALUES ($1, $2);', [name, score]);
 				const user = new Score(result.rows[0]);
 				resolve(user)
 			} catch (err) {
