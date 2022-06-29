8f8b98d4e09b904d9bcf90349c1cf74d43b99d6d
dartbill <billieldartnell@hotmail.com>
2022-06-29 09:42:58 +0100

    scoreboard route works

--- server/models/Score.js
+++ server/models/Score.js
@@ -9,17 +9,17 @@ class Score {
 	}
 
 	static get all() {
-		return new Promise(async (resolve, reject) => {
+		return new Promise(async (res, rej) => {
 			try {
-				const results = await db.query('SELECT * FROM scoreboard');
+				const results = await db.query('SELECT * FROM scoreBoard');
 				const scoreBoard = results.rows.map((s) => ({ id: s.id, name: s.name }));
 
 				// if (!scoreBoard.length) {
 				// 	throw new Error('No scores on the score board yet!');
 				// }
-				resolve(scoreBoard);
+				res(scoreBoard);
 			} catch (err) {
-				reject(`Error retrieving score board: ${err}`);
+				ref(`Error retrieving score board: ${err}`);
 			}
 		});
 	}
