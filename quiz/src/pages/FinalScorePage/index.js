4a68486f917f98be0873ce29bdcddfa3ca75babf
Rebecca Blixt <rebecca.blixt@hotmail.com>
2022-06-29 19:04:45 +0200

    fix react-confetti

--- quiz/src/pages/FinalScorePage/index.js
+++ quiz/src/pages/FinalScorePage/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { FinalScores } from '../../components';
 
-import useWindowSize from 'react-use/lib/useWindowSize';
 import Confetti from 'react-confetti';
 
 export const FinalScorePage = () => {
-	const { width, height } = useWindowSize();
 	return (
 		<>
 			<h2>Final Scores</h2>
-			<Confetti width={width} height={height} />
+			<Confetti
+				width={window.innerWidth || 300}
+				height={window.innerHeight || 200}
+			/>
 			<FinalScores />
 		</>
 	);
