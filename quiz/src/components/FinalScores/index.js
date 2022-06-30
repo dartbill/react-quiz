99d0951c05cee83b60387e5c28c235bfe3d59ade
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-06-30 01:03:55 +0100

    Prettify code

--- quiz/src/components/FinalScores/index.js
+++ quiz/src/components/FinalScores/index.js
@@ -3,11 +3,10 @@ import { useSelector } from "react-redux";
 import axios from "axios";
 
 export const FinalScores = () => {
-
     // fetch or get final scores from the database
     const player1 = useSelector((state) => state.player1);
     const player2 = useSelector((state) => state.player2);
-
+    const playerCount = useSelector((state) => state.playerCount);
     useEffect(() => {
         const postScores = async () => {
             try {
@@ -50,17 +49,30 @@ export const FinalScores = () => {
     return (
         <div>
             <p>Here are your scores: </p>
-            <ul>
-                <li>
-                    {player1.name} : {player1.score}
-                </li>
-                <li>
-                    {player2.name} : {player2.score}
-                </li>
-            </ul>
-            <p>{`Congratualtions ${
-                player1.score > player2.score ? player1.username : player2.username
-            }!`}</p>
+
+            {playerCount === 1 ? (
+                <ul>
+                    <li>
+                        {player1.username} : {player1.score}
+                    </li>
+                </ul>
+            ) : (
+                <>
+                    <ul>
+                        <li>
+                            {player1.username} : {player1.score}
+                        </li>
+                        <li>
+                            {player2.username} : {player2.score}
+                        </li>
+                    </ul>
+                    <p>{`Congratualtions ${
+                        player1.score > player2.score
+                            ? player1.username
+                            : player2.username
+                    }!`}</p>
+                </>
+            )}
         </div>
         //we also need a button that goes back
     );
