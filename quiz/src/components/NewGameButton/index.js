85ec5a99e6b49a1d5f9eb466cd73a8f1ce635f7e
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-07-01 00:49:47 +0100

    update NewGameButton to reset store

--- quiz/src/components/NewGameButton/index.js
+++ quiz/src/components/NewGameButton/index.js
@@ -1,11 +1,37 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
 
 export const NewGameButton = () => {
+    const currentState = useSelector(state => state);
+    console.log("state :",currentState)
+    
     let navigate = useNavigate();
+    const dispatch = useDispatch();
+
     const routeChange = (path) => {
+        // reset store
+        dispatch({type:"RESET"})
+        // go someplace special
         navigate(path);
+        
+        // dispatch({
+        //     type: "SET_PLAYER1",
+        //     payload: {
+        //       score: 0,
+        //     },
+        //   })
+    
+        //   dispatch({
+        //     type: "SET_PLAYER2",
+        //     payload: {
+        //       score: 0,
+        //     },
+        //   })
+
     };
+
 
     return (
         <button className="coolbutton " onClick={()=>routeChange("/")}>
