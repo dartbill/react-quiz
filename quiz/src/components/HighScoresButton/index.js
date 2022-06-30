e542afe034a5d6c83cf40363f2b05ed474a18e99
Tom Hughes <tomhughes1987@gmail.com>
2022-06-30 12:02:35 +0200

    various style bits

--- quiz/src/components/HighScoresButton/index.js
+++ quiz/src/components/HighScoresButton/index.js
@@ -8,7 +8,7 @@ export const HighScoresButton = () => {
   }
 
   return (
-    <button onClick={()=>routeChange('/highscores')}>HighScores</button>
+    <button className='btn' onClick={()=>routeChange('/highscores')}>HighScores</button>
   )
 }
 
