697e6c885a589fbf53a3e8a787e03a7d35f73fe3
Rebecca Blixt <rebecca.blixt@hotmail.com>
2022-06-30 18:05:33 +0200

    fix error after merging

--- quiz/src/actions/index.js
+++ quiz/src/actions/index.js
@@ -18,27 +18,14 @@ export const GetQuestions = (cat) => {
 			);
 			let resultsData = data.results;
 
-
-  if (cat.playerCount === 2) {
-    numOfq = 20
-  }
-
-  //fetch questions functions
-  return async (dispatch) => {
-    try {
-      const { data } = await axios.get(
-        `https://opentdb.com/api.php?&amount=${numOfq}&category=${category}&difficulty=${lev}&type=${type}`
-      );
-      let resultsData = data.results;
-
-      dispatch({
-        type: "TEST",
-        payload: shuffleArray(resultsData),
-      });
-    } catch (err) {
-      console.error(err)
-    }
-  };
+			dispatch({
+				type: 'TEST',
+				payload: shuffleArray(resultsData),
+			});
+		} catch (err) {
+			console.error(err);
+		}
+	};
 };
 
 //returns random question
