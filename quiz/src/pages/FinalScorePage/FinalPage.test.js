3125df9143558eca883d2b005079d77ea8075649
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-06-30 12:08:04 +0100

    WIP

--- quiz/src/pages/FinalScorePage/FinalPage.test.js
+++ quiz/src/pages/FinalScorePage/FinalPage.test.js
@@ -10,7 +10,7 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import "@testing-library/jest-dom";
-import { FinalScorePage } from ".";
+import { FinalScorePage } from "./index";
 import { useSelector } from "react-redux";
 import store from "../../store";
 import { BrowserRouter } from "react-router-dom";
