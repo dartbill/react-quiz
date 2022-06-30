52e5482f5a11fa3c8638bcf798d22e074b80bdc5
dartbill <billieldartnell@hotmail.com>
2022-06-30 13:45:40 +0100

    remove warning

--- quiz/src/components/Logo/Logo.js
+++ quiz/src/components/Logo/Logo.js
@@ -4,9 +4,9 @@ import './Logo.css'
 export default function Logo() {
   return (
     <div className='logo-Container'>
-    <img className='logo-Image' src={require('../../Images/logoNoText.png')} />
-    <h3 id='logo-Name'>Quiz-Nado</h3>
-    <p id='logo-Slogan'>Nuff Said</p>
+      <img className='logo-Image' alt='logo' src={require('../../Images/logoNoText.png')} />
+      <h3 id='logo-Name'>Quiz-Nado</h3>
+      <p id='logo-Slogan'>Nuff Said</p>
     </div>
   )
 }
