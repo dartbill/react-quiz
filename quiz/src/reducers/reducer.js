0345856e70c02f2550b9b1691ec4a6fb46d0b5ca
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-07-01 00:49:16 +0100

    update new reducer types

--- quiz/src/reducers/reducer.js
+++ quiz/src/reducers/reducer.js
@@ -5,62 +5,66 @@ const initialState = {
     playerCount: 1,
     questions: [],
     player1: {
-        name: "player1",
-        score: 0,
+      name: "player1",
+      score: 0,
     },
     player2: {
-        name: "player2",
-        score: 0,
+      name: "player2",
+      score: 0,
     },
-};
-
-const reducer = (state = initialState, action) => {
+    currentQuestionIndex: 0,
+  
+  };
+  
+  const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case "SETCAT":
-            return {
-                ...state,
-                category: action.payload,
-            };
-        case "SETLEVEL":
-            return {
-                ...state,
-                level: action.payload,
-            };
-        case "SETTYPE":
-            return {
-                ...state,
-                typeOfQ: action.payload,
-            };
-        case "TEST":
-            return {
-                ...state,
-                questions: action.payload,
-            };
-        case "SET_PLAYER_COUNT":
-            return {
-                ...state,
-                playerCount: action.payload,
-            };
-        case "SET_PLAYER1":
-            return {
-                ...state,
-                player1: {
-                    ...state.player1,
-                    ...action.payload,
-                },
-            };
-        case "SET_PLAYER2":
-            return {
-                ...state,
-                player2: {
-                    ...state.player2,
-                    ...action.payload,
-                },
-            };
-
-        default:
-            return initialState;
+      case "SETCAT":
+        return {
+          ...state,
+          category: action.payload,
+        };
+      case "SETLEVEL":
+        return {
+          ...state,
+          level: action.payload,
+        };
+      case "SETTYPE":
+        return {
+          ...state,
+          typeOfQ: action.payload,
+        };
+      case "TEST":
+        return {
+          ...state,
+          questions: action.payload,
+        };
+      case "SET_PLAYER_COUNT":
+        return {
+          ...state,
+          playerCount: action.payload,
+        };
+      case "SET_PLAYER1":
+        return {
+          ...state,
+          player1: {
+            ...state.player1,
+            ...action.payload,
+          },
+        };
+      case "SET_PLAYER2":
+        return {
+          ...state,
+          player2: {
+            ...state.player2,
+            ...action.payload,
+          },
+        };
+      case "NEXT_QUESTION":
+        return { ...state, currentQuestionIndex: state.currentQuestionIndex + 1 };
+  
+      default:
+        return { ...initialState };
     }
-};
-
-export default reducer;
+  };
+  
+  export default reducer;
