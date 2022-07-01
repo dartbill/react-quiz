f45b23c6a54dae9a8fd3adb6f772e629a581b484
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-07-01 02:01:16 +0100

    clean up code in NewGameButton comp

--- quiz/src/components/NewGameButton/index.js
+++ quiz/src/components/NewGameButton/index.js
@@ -1,21 +1,16 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
 export const NewGameButton = () => {
-    const currentState = useSelector(state => state);
-    console.log("state :",currentState)
-    
     let navigate = useNavigate();
     const dispatch = useDispatch();
-
     const routeChange = (path) => {
         // reset store
         dispatch({type:"RESET"})
         // go someplace special
         navigate(path);
     };
-
 
     return (
         <button className="coolbutton " onClick={()=>routeChange("/")}>
