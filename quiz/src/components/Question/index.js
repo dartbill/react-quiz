5759c45cbae929a0ca2b46ae58edf16beebcb3be
Tom Hughes <tomhughes1987@gmail.com>
2022-07-01 09:19:41 +0200

    reformat the questions and answer page

--- quiz/src/components/Question/index.js
+++ quiz/src/components/Question/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { Answer } from "../";
+import './Questions.css'
 
 export const Question = ({ index, onSubmitQuestion }) => {
 
