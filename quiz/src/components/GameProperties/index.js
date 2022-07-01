7d246fabb81ffbe4d2bedd28f81f208bf7db7934
Rebecca Blixt <rebecca.blixt@hotmail.com>
2022-07-01 14:01:45 +0200

    change category name

--- quiz/src/components/GameProperties/index.js
+++ quiz/src/components/GameProperties/index.js
@@ -1,133 +1,123 @@
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
-import "./index.css";
+import './index.css';
 
 export const GameProperties = () => {
-  let [diffBtnText, setDiffBtnText] = useState("Difficulty");
-  let [catBtnText, setCatBtnText] = useState("Category");
-  let [typeBtnText, setTypeBtnText] = useState("Type");
+	let [diffBtnText, setDiffBtnText] = useState('Difficulty');
+	let [catBtnText, setCatBtnText] = useState('Category');
+	let [typeBtnText, setTypeBtnText] = useState('Type');
 
-  const dispatch = useDispatch();
-  const handleBtnClick = (e) => {
-    e.preventDefault();
-    setCatBtnText(e.currentTarget.innerHTML);
-    dispatch({ type: "SETCAT", payload: e.target.id });
-  };
+	const dispatch = useDispatch();
+	const handleBtnClick = (e) => {
+		e.preventDefault();
+		setCatBtnText(e.currentTarget.innerHTML);
+		dispatch({ type: 'SETCAT', payload: e.target.id });
+	};
 
-  const setDifficulty = (e) => {
-    e.preventDefault();
-    setDiffBtnText(e.currentTarget.id);
-    dispatch({ type: "SETLEVEL", payload: e.target.id });
-  };
+	const setDifficulty = (e) => {
+		e.preventDefault();
+		setDiffBtnText(e.currentTarget.id);
+		dispatch({ type: 'SETLEVEL', payload: e.target.id });
+	};
 
-  const handleClickType = (e) => {
-    e.preventDefault();
-    setTypeBtnText(e.currentTarget.innerHTML);
-    dispatch({ type: "SETTYPE", payload: e.target.id });
-  };
+	const handleClickType = (e) => {
+		e.preventDefault();
+		setTypeBtnText(e.currentTarget.innerHTML);
+		dispatch({ type: 'SETTYPE', payload: e.target.id });
+	};
 
-  return (
-    <div id="gameProperties">
-      <div className="dropdown">
-        <button className="dropbtn-diff drop">{diffBtnText}</button>
-        <div className="dropdown-content-diff">
-          <a
-            href="/#"
-            onClick={setDifficulty}
-            className="drop-items-diff drop-items drop"
-            id="easy"
-          >
-           Easy
-          </a>
-          <a
-            href="/#"
-            onClick={setDifficulty}
-            className="drop-items-diff drop-items drop"
-            id="medium"
-          >
-            Medium
-          </a>
-          <a
-            href="/#"
-            onClick={setDifficulty}
-            className="drop-items-diff drop-items drop"
-            id="hard"
-          >
-            Hard
-          </a>
-        </div>
-      </div>
+	return (
+		<div id='gameProperties'>
+			<div className='dropdown'>
+				<button className='dropbtn-diff drop'>{diffBtnText}</button>
+				<div className='dropdown-content-diff'>
+					<a
+						href='/#'
+						onClick={setDifficulty}
+						className='drop-items-diff drop-items drop'
+						id='easy'>
+						Easy
+					</a>
+					<a
+						href='/#'
+						onClick={setDifficulty}
+						className='drop-items-diff drop-items drop'
+						id='medium'>
+						Medium
+					</a>
+					<a
+						href='/#'
+						onClick={setDifficulty}
+						className='drop-items-diff drop-items drop'
+						id='hard'>
+						Hard
+					</a>
+				</div>
+			</div>
 
-      <div className="dropdown">
-        <button className="dropbtn-cat drop">{catBtnText}</button>
-        <div className="dropdown-content-cat">
-          <a
-            href="/#"
-            onClick={handleBtnClick}
-            className="drop-items-cat drop-items drop"
-            id="9"
-          >
-            General knowledge
-          </a>
-          <a
-            href="/#"
-            onClick={handleBtnClick}
-            className="drop-items-cat drop-items drop"
-            id="11"
-          >
-            Films
-          </a>
-          <a
-            href="/#"
-            onClick={handleBtnClick}
-            className="drop-items-cat drop-items drop"
-            id="15"
-          >
-            Games
-          </a>
-          <a
-            href="/#"
-            onClick={handleBtnClick}
-            className="drop-items-cat drop-items drop"
-            id="27"
-          >
-            Animals
-          </a>
-          <a
-            href="/#"
-            onClick={handleBtnClick}
-            className="drop-items-cat drop-items  drop"
-            id="18"
-          >
-            Computer Shit
-          </a>
-        </div>
-      </div>
+			<div className='dropdown'>
+				<button className='dropbtn-cat drop'>{catBtnText}</button>
+				<div className='dropdown-content-cat'>
+					<a
+						href='/#'
+						onClick={handleBtnClick}
+						className='drop-items-cat drop-items drop'
+						id='9'>
+						General knowledge
+					</a>
+					<a
+						href='/#'
+						onClick={handleBtnClick}
+						className='drop-items-cat drop-items drop'
+						id='11'>
+						Films
+					</a>
+					<a
+						href='/#'
+						onClick={handleBtnClick}
+						className='drop-items-cat drop-items drop'
+						id='15'>
+						Games
+					</a>
+					<a
+						href='/#'
+						onClick={handleBtnClick}
+						className='drop-items-cat drop-items drop'
+						id='27'>
+						Animals
+					</a>
+					<a
+						href='/#'
+						onClick={handleBtnClick}
+						className='drop-items-cat drop-items  drop'
+						id='18'>
+						Computer Science
+					</a>
+				</div>
+			</div>
 
-      <div className="dropdown">
-        <button className="dropbtn-type drop">{typeBtnText}</button>
-        <div className="dropdown-content-cat">
-          <a
-            href="/#"
-            className="drop-items-type drop-items drop"
-            id="boolean"
-            onClick={handleClickType}
-          >
-            True/False
-          </a>
-          <a
-            href="/#"
-            className="drop-items-type drop-items drop"
-            id="multiple"
-            onClick={handleClickType}
-          >
-            Multiple
-          </a>
-        </div>
-      </div>
-    </div>
-  );
+			<div className='dropdown'>
+				<button className='dropbtn-type drop'>{typeBtnText}</button>
+				<div className='dropdown-content-cat'>
+					<a
+						href='/#'
+						className='drop-items-type drop-items drop'
+						id='boolean'
+						onClick={handleClickType}>
+						True/False
+					</a>
+					<a
+						href='/#'
+						className='drop-items-type drop-items drop'
+						id='multiple'
+						onClick={handleClickType}>
+						Multiple
+					</a>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 // use the category num in the id=''
